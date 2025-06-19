3d33a27cae06749eb7266497264a8d3536850e86
lansuri <lansurisuresh1612@gmail.com>
2025-06-19 19:03:05 +0530

    Add files via upload

---
+++ assests/file.js
@@ -0,0 +1,334 @@
+all_products=[
+    {
+      Id:1,
+      Img:"https://img.freepik.com/free-photo/delicious-ice-cream-with-topping_23-2150735434.jpg?uid=R191274230&ga=GA1.1.152707082.1741785601&semt=ais_items_boosted&w=740",
+      Name:"butterscoch",
+      Price:170,
+    },
+    {
+      Id:2,
+      Img:"https://img.freepik.com/free-psd/close-up-delicious-ice-cream_23-2150115057.jpg?uid=R191274230&ga=GA1.1.152707082.1741785601&semt=ais_items_boosted&w=740",
+      Name:"chocolate",
+      Price:200,
+    },
+    {
+      Id:3,
+      Img:"https://img.freepik.com/free-photo/side-view-chocolate-ice-cream-with-nuts-wafer-rolls_140725-11439.jpg?uid=R191274230&ga=GA1.1.152707082.1741785601&semt=ais_items_boosted&w=740",
+      Name:"cookie and cream",
+      Price:230, 
+    },
+    {
+      Id:4,
+      Img:"https://img.freepik.com/premium-photo/vegan-beets-ice-cream-with-chocolate-drops_136595-119.jpg?uid=R191274230&ga=GA1.1.152707082.1741785601&semt=ais_items_boosted&w=740",
+      Name:"watermelon",
+      Price:130,
+    },
+    {
+      Id:5,
+      Img:"https://img.freepik.com/free-photo/delicious-ice-cream-studio_23-2150696825.jpg?uid=R191274230&ga=GA1.1.152707082.1741785601&semt=ais_items_boosted&w=740",
+      Name:"vannila",
+      Price:100,
+    },
+   
+    {
+      Id:6,
+      Img:"https://img.freepik.com/premium-photo/healthy-homemade-strawberry-sorbet-dairy-free-gluten-free-without-sugar_91908-3400.jpg?uid=R191274230&ga=GA1.1.152707082.1741785601&semt=ais_items_boosted&w=740",
+      Name:"straberry",
+      Price:150,
+    },
+    {
+      Id:7,
+      Img:"https://img.freepik.com/premium-photo/bowl-with-ice-cream-balls-ingredients-black-smokey-table_185193-38825.jpg?uid=R191274230&ga=GA1.1.152707082.1741785601&semt=ais_items_boosted&w=740",
+      Name:"mango",
+      Price:200,
+    },
+    {
+      Id:8,
+      Img:"https://img.freepik.com/free-photo/bowl-with-delicious-ice-cream-cookies_23-2149681929.jpg?uid=R191274230&ga=GA1.1.152707082.1741785601&semt=ais_items_boosted&w=740",
+      Name:"oreo",
+      Price:270, 
+    },
+    {
+      Id:9,
+      Img:"https://img.freepik.com/free-photo/raspberry-ice-cream-cup_23-2151991273.jpg?uid=R191274230&ga=GA1.1.152707082.1741785601&semt=ais_hybrid&w=740",
+      Name:"Rasberry",
+      Price:200, 
+    },
+    {
+      Id:10,
+      Img:"https://img.freepik.com/free-photo/melting-ice-cream-cone_23-2150373267.jpg?uid=R191274230&ga=GA1.1.152707082.1741785601&semt=ais_items_boosted&w=740",
+      Name:"mixed-flavor",
+      Price:300,
+    },
+    {
+      Id:11,
+      Img:"https://img.freepik.com/free-photo/delicious-ice-cream-studio_23-2150696576.jpg?uid=R191274230&ga=GA1.1.152707082.1741785601&semt=ais_items_boosted&w=740",
+      Name:"pisthasio",
+      Price:320, 
+    },
+    {
+      Id:12,
+      Img:"https://img.freepik.com/free-photo/delicious-ice-cream-with-topping_23-2150735490.jpg?uid=R191274230&ga=GA1.1.152707082.1741785601&semt=ais_items_boosted&w=740",
+      Name:"blackcurrent with butterscoch",
+      Price:350,
+    },
+    {
+      Id:13,
+      Img:"https://img.freepik.com/free-photo/view-delicious-frozen-ice-cream-dessert-with-bananas_23-2150822990.jpg?uid=R191274230&ga=GA1.1.152707082.1741785601&semt=ais_hybrid&w=740",
+      Name:"Banana",
+      Price:50,
+    },
+    {
+      Id:14,
+      Img:"https://img.freepik.com/free-photo/flat-lay-ice-cream-stick-plate-with-ice-cubes_23-2148422041.jpg?uid=R191274230&ga=GA1.1.152707082.1741785601&semt=ais_items_boosted&w=740",
+      Name:"grape popsicles",
+      Price:30,
+    },
+    {
+      Id:15,
+      Img:"https://img.freepik.com/free-photo/vertical-shot-home-made-vegan-icecream-covered-by-chocolate-surrounded-by-ice-cubes_181624-16650.jpg?uid=R191274230&ga=GA1.1.152707082.1741785601&semt=ais_items_boosted&w=740",
+      Name:"choco bar",
+      Price:70, 
+    },
+    {
+      Id:16,
+      Img:"https://img.freepik.com/free-photo/delicious-coconut-ice-cream-pop-stickles_23-2149460219.jpg?uid=R191274230&ga=GA1.1.152707082.1741785601&semt=ais_items_boosted&w=740",
+      Name:"coconut",
+      Price:60,
+    },
+    {
+      Id:17,
+      Img:"https://img.freepik.com/free-photo/assortment-fruit-popsicles-with-kiwi_23-2148430623.jpg?uid=R191274230&ga=GA1.1.152707082.1741785601&semt=ais_items_boosted&w=740",
+      Name:"kiwi popsicles",
+      Price:90, 
+    },
+    {
+      Id:18,
+      Img:"https://img.freepik.com/free-photo/typical-brazilian-freezies-still-life_23-2150171738.jpg?uid=R191274230&ga=GA1.1.152707082.1741785601&semt=ais_hybrid&w=740",
+      Name:"Pepsi",
+      Price:100,
+    },
+    {
+      Id:19,
+      Img:"https://img.freepik.com/free-photo/still-life-cookies-ice-cream_23-2149637125.jpg?uid=R191274230&ga=GA1.1.152707082.1741785601&semt=ais_hybrid&w=740",
+      Name:"Straberry and oreo",
+      Price:150,
+    },
+    {
+      Id:20,
+      Img:"https://img.freepik.com/free-photo/side-view-thai-ice-cream-roll-whipped-cream-decorated-with-colorful-sprinkles-cardboard-bowl-black-wall_140725-12927.jpg?uid=R191274230&ga=GA1.1.152707082.1741785601&semt=ais_hybrid&w=740",
+      Name:"Blueberry roll",
+      Price:200, 
+    },
+    {
+      Id:21,
+      Img:"https://img.freepik.com/free-photo/top-view-bowl-with-ice-cream-with-cubes_23-2148505879.jpg?uid=R191274230&ga=GA1.1.152707082.1741785601&semt=ais_items_boosted&w=740",
+      Name:"lemon popsicles",
+      Price:100,
+    },
+    {
+      Id:22,
+      Img:"https://img.freepik.com/free-photo/delicious-ice-cream-with-topping_23-2150735498.jpg?uid=R191274230&ga=GA1.1.152707082.1741785601&semt=ais_hybrid&w=740",
+      Name:"Cherry",
+      Price:250, 
+    },
+    {
+      Id:23,
+      Img:"https://img.freepik.com/free-photo/delicious-granita-dessert-with-peach-high-angle_23-2149395612.jpg?uid=R191274230&ga=GA1.1.152707082.1741785601&semt=ais_hybrid&w=740",
+      Name:"Peach",
+      Price:50,
+    },
+    {
+      Id:24,
+      Img:"https://img.freepik.com/free-photo/composition-delicious-homemade-icecream_23-2148900956.jpg?uid=R191274230&ga=GA1.1.152707082.1741785601&semt=ais_hybrid&w=740",
+      Name:"Saffron",
+      Price:350,
+    },
+    
+  ]
+     
+
+
+ function fun(event){
+       event.preventDefault()
+       var Username1 = document.getElementById("username").value;
+       var password1 = document.getElementById("password").value;
+
+       if(Username1 ==="lansu" && password1 === "Abc@2003"){
+        window.location.href="./assests/addtocart.html"
+       }else{
+          document.getElementById("result").innerHTML="INVALID"
+       }
+    }
+  function toggleicon(){
+         var passwordIcon=document.getElementById("password")
+        if(passwordIcon.type==="password"){
+            passwordIcon.type="text"
+            document.getElementById("show").style.display="none";
+            document.getElementById("hide").style.display="inline";
+        }else{
+            passwordIcon.type="password"
+            document.getElementById("show").style.display="inline";
+            document.getElementById("hide").style.display="none";
+        }
+
+    }
+  function password_fun(){
+        var password_update=document.getElementById("password").value
+
+        var firstletter=password_update.charAt(0)
+         
+        if(firstletter===firstletter.toUpperCase()){
+           document.getElementById("uppercase").innerHTML="FIRST LETTER MUST BE CAPITAL" 
+           document.getElementById("uppercase").style.color="green"
+
+        }else{
+            document.getElementById("uppercase").innerHTML="FIRST LETTER MUST BE CAPITAL" 
+            document.getElementById("uppercase").style.color="red"
+        }
+
+        if(password_update.length>=8){
+            document.getElementById("eightcharacters").innerHTML="MINIMUM 8 CHARACTERS"
+            document.getElementById("eightcharacters").style.color="green"
+        }else{
+            document.getElementById("eightcharacters").innerHTML="MINIMUM 8 CHARACTERS"
+            document.getElementById("eightcharacters").style.color="red"
+        }
+
+        var special1=["!" ,"@" ,"#" ,"$" ,"%" ,"&", "*"] 
+        if(special1.some(a=>password_update.includes(a))){
+            document.getElementById("special").innerHTML="ANY ONE SPECIAL CHARACTERS"
+            document.getElementById("special").style.color="green"
+        } else{
+            document.getElementById("special").innerHTML="ANY ONE SPECIAL CHARACTERS"
+            document.getElementById("special").style.color="red"
+        }
+
+        var number1=["0","1","2","3","4","5","6","7","8","9"]
+        if(number1.some(a=>password_update.includes(a))){
+            document.getElementById("number").innerHTML="ANY ONE NUMBER"
+            document.getElementById("number").style.color="green"
+        }else{
+             document.getElementById("number").innerHTML="ANY ONE NUMBER"
+             document.getElementById("number").style.color="red" 
+        }
+    }
+
+function DisplayProducts(){
+  var content=""
+   all_products.map((product)=>{
+    content+=`
+     <div class="col-lg-3 col-md-6 mt-3">
+        <div class="card">
+          <img src="${product.Img}" height="300" p- y-4 alt="">
+          <div class="card-body">
+            <h5>${product.Name}</h5>
+             <p>Price:₹${product.Price}</p>
+            <button class="btn btn-primary w-100" onclick="Addtocart(${product.Id})">Add To Cart</button>
+          </div>
+        </div>
+      </div>
+    `
+   })
+   document.getElementById("card-row").innerHTML=content
+}
+DisplayProducts()
+
+var cart=[];
+var count=0;
+var total_price=0
+
+function cart_count(){
+  document.getElementById("cart").innerHTML = count;
+  document.getElementById("cart").style.display = count === 0 ? "none" : "inline";
+}
+ 
+
+function Addtocart(productID){
+  var productData = all_products.find(a=>a.Id === productID);
+  var existingProduct = cart.find(a=>a.Id === productID)
+
+  if(existingProduct){
+    existingProduct.quantity++
+  }
+  else{
+    productData.quantity= 1 
+    cart.push(productData);
+  }
+  count++;
+ cart_count()
+ Displaycart(cart)
+ total_price = productData.Price + total_price
+ document.getElementById("totalprice").innerHTML=`Total Price : ₹${total_price}`
+ 
+}
+
+
+function Displaycart(cartData){
+  var content=""
+  if(cartData.length === 0){
+    content+=`
+     <tr>
+        <td colspan= "5" class="text-danger text-align-center"> Your card id empty..!</td>
+     </tr>
+    `
+  }
+  else{
+    cartData.map((product)=>{
+      content+=`
+      <tr>
+        <td>
+          <img src="${product.Img}" height="120px"alt="">
+        </td>
+          <td>${product.Name}</td>
+          <td>${product.Price}</td>
+          <td>${product.quantity}</td>
+          <td><button class="btn btn-danger" onclick="Removefun(${product.Id})">Remove</button></td>
+      </tr>
+      `
+    })
+  }
+  document.getElementById("cart-remove").innerHTML=content;
+}
+function Removefun(productID){
+  var product=all_products.find(a=>a.Id ===  productID)
+   if(product.quantity>1){
+    product.quantity--;
+   }
+   else{
+    cart=cart.filter(a=>a.Id !== productID)
+   }
+   count--;
+   cart_count()
+
+   total_price=total_price - product.Price
+   document.getElementById("totalprice").innerHTML=`Total Price:₹${total_price}` 
+   Displaycart(cart)
+}
+// ===============search=========================================
+   
+  const searchinput=(data)=>{
+    var content1=""
+    data.map((product)=>{
+      content1+=`
+       <div class="col-lg-3 col-md-6 mt-3">
+        <div class="card">
+          <img src="${product.Img}" height="300" class="py-4" alt="">
+          <div class="card-body">
+            <h5>${product.Name}</h5>
+             <p>Price:₹${product.Price}</p>
+            <button class="btn btn-primary w-100" onclick="Addtocart(${product.Id})">Add To Cart</button>
+          </div>
+        </div>
+      </div>
+      `
+    })
+    document.getElementById("search").innerHTML=content1
+  }
+  searchinput(all_products)
+  const fun1=(event)=>{
+    event.preventDefault()
+    var box=document.getElementById("modalsearch").value.toLowerCase();
+    var a = all_products.filter(b=>b.Name.toLowerCase().includes(box) || b.Price.toString().includes(box))
+    searchinput(a)
+  }
